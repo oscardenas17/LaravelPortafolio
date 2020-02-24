0886339fb03655b623e2762e7b308d59218c3ae8
oscardenas17 <oscardenas17@gmail.com>
2020-02-23 22:02:28 -0500

    config mail for production-  mails con sendgrid

--- config/services.php
+++ config/services.php
@@ -24,6 +24,11 @@ return [
         'token' => env('POSTMARK_TOKEN'),
     ],
 
+    /*sendgrid mail*/
+    'sendgrid' => [
+        'api_key' => env('SENDGRID_API_KEY'),
+    ],
+
     'ses' => [
         'key' => env('AWS_ACCESS_KEY_ID'),
         'secret' => env('AWS_SECRET_ACCESS_KEY'),
